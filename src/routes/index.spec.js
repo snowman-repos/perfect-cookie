6b47f6de11c38240bd3ebc3b2428688ae8a6ee95
Darryl <dazsnow@gmail.com>
2020-04-02 22:20:28 +0800

    add jest

---
+++ src/routes/index.spec.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/svelte";
+import Index from "./index.svelte";
+
+describe("index component", () => {
+  test("should render component correctly", () => {
+    const { container } = render(Index);
+
+    expect(container).toContainHTML("<h1>Great success!</h1>");
+  });
+});
