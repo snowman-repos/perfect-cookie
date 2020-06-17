fa1b843b2866a8529478ed313b393d358ca90a9d
Darryl <dazsnow@gmail.com>
2020-06-17 23:27:24 +0800

    add intro paragraph

--- README.md
+++ README.md
@@ -2,14 +2,40 @@
 
 Generate the recipe for _your_ perfect cookie.
 
-## Build with:
+## Built with:
 
-- Svelte
-- Sapper
+- [https://svelte.dev/](Svelte)
+- [https://sapper.svelte.dev/](Sapper)
 - [https://github.com/hperrin/svelte-material-ui](Svelte Material UI) (SMUI)
-- Jest
+- [https://jestjs.io/](Jest)
+
+## Background
+
+Why I made it
+How I made it
+
+## Credits & References
+
+Inspiration
+Recipe sources
+
+## Help / Contribute
+
+I would love to have an image or illustration at the top of the page which depicts what the cookie might look like, based on the selected properties. I'm imagining a top-down view and a cross section that shows the color, mouthfeel, surface quality, texture, and thickness. Given how many permutations there are, sourcing images and loading/updating them each time any slider is changed might not be feasible. I thought about animating SVGs or drawing something with canvas that can be manipulated by the code might work, but not sure how to implement it. If anyone has any idea then please [mailto:darryl@darrylsnow.com](get in touch with me).
+
+If there are any Math geniuses who want to help out, right now I'm just using linear equations in the calculations, but they don't exactly fit the model from my data...
 
 ## TODO
 
 - [ ] Either remove SMUI or fix it so that events can be tested (See [https://github.com/hperrin/svelte-material-ui/issues/110](issue))
-- [ ] Fix test for Method component that requires waiting for store to be updated
+- [ ] Fix tests with Timeouts for waiting for the store to be updated
+- [ ] Add a feedback form
+- [ ] Add open graph data
+- [ ] Share buttons
+- [ ] Add rich data (schema.org)
+- [ ] Print styles and print buttons
+- [ ] Tips & insights on how the ingredients and method affect the cookie properties
+- [ ] Donate button (buymeacoffee)
+- [ ] About page
+- [ ] Rip out Cypress (unused)
+- [ ] Image for the header
--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -1,3 +1,27 @@
+<div class="mdc-layout-grid">
+  <div class="grid mdc-layout-grid__inner">
+    <div class="first-column mdc-layout-grid__cell mdc-layout-grid__cell--span-1-phone mdc-layout-grid__cell--span-1-tablet mdc-layout-grid__cell--span-2-desktop">
+      <span>{from}</span>
+    </div>
+    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2-phone mdc-layout-grid__cell--span-6-tablet mdc-layout-grid__cell--span-8-desktop">
+      <Slider bind:value={$recipe.properties[property]} min={0} max={100} step={10} on:MDCSlider:change={update} />
+    </div>
+    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-1-phone mdc-layout-grid__cell--span-1-tablet mdc-layout-grid__cell--span-2-desktop">
+      <span>{to}</span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .grid {
+    align-items: center
+  }
+
+  .first-column {
+    text-align: right
+  }
+</style>
+
 <script>
   import { getContext } from 'svelte'
   import Slider from '@smui/slider/bare.js'
@@ -15,27 +39,3 @@
     debounce(updateRecipe(property), 1000)
   }
 </script>
-
-<style>
-  .grid {
-    align-items: center
-  }
-
-  .first-column {
-    text-align: right
-  }
-</style>
-
-<div class="mdc-layout-grid">
-  <div class="grid mdc-layout-grid__inner">
-    <div class="first-column mdc-layout-grid__cell mdc-layout-grid__cell--span-1-phone mdc-layout-grid__cell--span-1-tablet mdc-layout-grid__cell--span-2-desktop">
-      <span>{from}</span>
-    </div>
-    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-2-phone mdc-layout-grid__cell--span-6-tablet mdc-layout-grid__cell--span-8-desktop">
-      <Slider bind:value={$recipe.properties[property]} min={0} max={100} step={10} on:MDCSlider:change={update} />
-    </div>
-    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-1-phone mdc-layout-grid__cell--span-1-tablet mdc-layout-grid__cell--span-2-desktop">
-      <span>{to}</span>
-    </div>
-  </div>
-</div>
